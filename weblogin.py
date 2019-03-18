bfebf361431bc9d0dec8da9d4a2d9c638e2dabbc
christian <46280546+christianmw24@users.noreply.github.com>
2019-03-18 19:59:39 +0100

    Add files via upload

---
+++ weblogin.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get('http://seleniumhq.org/')
+
+
+
+# try github3.py recorded 20190227 works 20190227 cm
+# try yahoo1.de recorded 20190227 works 20190227 cm
